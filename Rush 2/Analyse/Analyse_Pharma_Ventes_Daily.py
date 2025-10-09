30a402fe28f7e7d2b2a6cffd47c07d74ddcf6401
StefanB35 <beaulieu.stefan.2004@gmail.com>
2025-10-09 17:15:02 +0200

    mini analyse

---
+++ Rush 2/Analyse/Analyse_Pharma_Ventes_Daily.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Charger le fichier nettoyé
+df = pd.read_csv('Rush 2/Cleaned_data/Pharma_Ventes_Daily_Clean.csv')
+
+# Afficher les premières lignes
+print("Aperçu des données :")
+print(df.head())
+
+# Infos générales
+print("\nInfos générales :")
+print(df.info())
+
+# Statistiques descriptives
+print("\nStatistiques descriptives :")
+print(df.describe())
+
+# Valeurs manquantes
+print("\nValeurs manquantes par colonne :")
+print(df.isnull().sum())
